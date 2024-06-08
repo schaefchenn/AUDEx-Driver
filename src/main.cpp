9d5c5a811c0dabc80c97ceab36bbb85ab1a0dc0a
真夜中の雰囲気午夜氛圍 <marcelka218@gmail.com>
2024-06-09 00:44:18 +0200

    Steering functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,34 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-// create servo object
-//Servo absimaServo;
-//int absimaServoPin = 15;
+#define motorPin 26
+#define steeringPin 18
 
+// create servo object
+Servo absimaServo;
 Servo absimaMotor;
-int absimaMotorPin = 15;
 
 void setup() {
   Serial.begin(115200);
-  //absimaServo.attach(absimaServoPin); // Attach the servo after it has been detached
+  absimaServo.attach(steeringPin); // Attach the servo after it has been detached
+  //absimaMotor.setPeriodHertz(90);      // Standard 50hz servo
+  absimaMotor.attach(motorPin); // Attach the servo after it has been detached
 }
 
 void loop() {
   absimaMotor.write(90);
   // Move servo from 0 to 180 degrees
-  /*
+
   for (int i = 0; i <= 180; i += 1) {
-    //absimaServo.write(i);
+    absimaServo.write(i);
     delay(10); // Adjust delay for desired speed
 
-    //Serial.print("degrees: ");
-    //Serial.print(i);
-    //Serial.println(" °");
+    Serial.print("degrees: ");
+    Serial.print(i);
+    Serial.println(" °");
   }
+
+  delay(1000); // Add a delay between cycles (optional)
 
   // Move servo back from 180 to 0 degrees
   for (int i = 180; i >= 0; i -= 1) {
@@ -37,5 +41,4 @@ void loop() {
   }
 
   delay(1000); // Add a delay between cycles (optional)
-  */
 }
