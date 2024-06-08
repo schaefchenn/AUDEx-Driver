bbec0037b05408c359a42cdae41fb5876d8bd059
真夜中の雰囲気午夜氛圍 <marcelka218@gmail.com>
2024-06-08 02:35:40 +0200

    added files

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+// create servo object
+Servo absimaServo;
+int absimaServoPin = 15;
+
+void setup() {
+  Serial.begin(115200);
+  absimaServo.attach(absimaServoPin); // Attach the servo after it has been detached
+}
+
+void loop() {
+  // Move servo from 0 to 180 degrees
+  for (int i = 0; i <= 180; i += 1) {
+    absimaServo.write(i);
+    delay(10); // Adjust delay for desired speed
+
+    Serial.print("degrees: ");
+    Serial.print(i);
+    Serial.println(" °");
+  }
+
+  // Move servo back from 180 to 0 degrees
+  for (int i = 180; i >= 0; i -= 1) {
+    absimaServo.write(i);
+    delay(10); // Adjust delay for desired speed
+
+    Serial.print("degrees: ");
+    Serial.print(i);
+    Serial.println(" °");
+  }
+
+  delay(1000); // Add a delay between cycles (optional)
+}
