27d7cc778c4f1a30b5e71ce7ef380d72cddd8dc3
真夜中の雰囲気午夜氛圍 <marcelka218@gmail.com>
2024-06-09 15:44:04 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
+
 #define motorPin 26
-#define steeringPin 18
+#define steeringPin 25
 
 // create servo object
 Servo absimaServo;
@@ -11,15 +12,21 @@ Servo absimaMotor;
 void setup() {
   Serial.begin(115200);
   absimaServo.attach(steeringPin); // Attach the servo after it has been detached
-  //absimaMotor.setPeriodHertz(90);      // Standard 50hz servo
+
+  
+  // pinMode(motorPin, OUTPUT);
+  //absimaMotor.setPeriodHertz(3000);      // Standard 50hz servo
   absimaMotor.attach(motorPin); // Attach the servo after it has been detached
+  absimaMotor.write(0);
+  delay(2000);
 }
 
 void loop() {
   absimaMotor.write(90);
   // Move servo from 0 to 180 degrees
-
-  for (int i = 0; i <= 180; i += 1) {
+  
+  
+  for (int i = 30; i <= 150; i += 1) {
     absimaServo.write(i);
     delay(10); // Adjust delay for desired speed
 
@@ -31,7 +38,7 @@ void loop() {
   delay(1000); // Add a delay between cycles (optional)
 
   // Move servo back from 180 to 0 degrees
-  for (int i = 180; i >= 0; i -= 1) {
+  for (int i = 150; i >= 30; i -= 1) {
     absimaServo.write(i);
     delay(10); // Adjust delay for desired speed
 
