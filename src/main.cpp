f9b1f96c311835b009af88ee27f5d197157343ad
真夜中の雰囲気午夜氛圍 <marcelka218@gmail.com>
2024-06-10 18:43:28 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,22 @@ Servo absimaServo;
 Servo absimaMotor;
 
 void setup() {
-  Serial.begin(115200);
-  absimaServo.attach(steeringPin); // Attach the servo after it has been detached
+  Serial.begin(115200); // Attach the servo after it has been detached
+  absimaServo.attach(steeringPin);
 
-  
-  // pinMode(motorPin, OUTPUT);
-  //absimaMotor.setPeriodHertz(3000);      // Standard 50hz servo
   absimaMotor.attach(motorPin); // Attach the servo after it has been detached
-  absimaMotor.write(0);
-  delay(2000);
+  absimaMotor.writeMicroseconds(1500);
+  delay(10000); // wait 10s
+  Serial.println("Setup done");
+
 }
 
 void loop() {
-  absimaMotor.write(90);
+  //absimaMotor.write(90);
+  absimaMotor.writeMicroseconds(1600);
+  
+  
   // Move servo from 0 to 180 degrees
-  
-  
   for (int i = 30; i <= 150; i += 1) {
     absimaServo.write(i);
     delay(10); // Adjust delay for desired speed
